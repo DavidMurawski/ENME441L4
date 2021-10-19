f95416fe278ce108afa175ab6a17c3a72659f86c
David Murawski <91499129+DavidMurawski@users.noreply.github.com>
2021-10-19 03:25:19 +0000

    j

--- lab4.py
+++ lab4.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python27all
+#!/usr/bin/python37all
 import cgi
 import json
+
+import cgitb
+cgitb.enable()
 
 #green blue red
 leds = [0, 0, 0]
@@ -16,3 +19,14 @@ ledjson = {"green":leds[0], "blue":leds[1], "red":leds[2]}
 
 with open('lab4.txt', 'w') as f:  
   json.dump(ledjson, f)
+
+"""
+print('Content-type: text/html\n\n')
+print('<html>')
+print('<form action="/cgi-bin/led-pwm-multiple.py" method="POST">')
+print('<input type="range" name="slider" min="0" max="100" value="%s"><br>' % brightness)
+print('<input type="submit" value="Change LED brightness">')
+print('</form>')
+print('Brightness = %s' % brightness)
+print('</html>')
+"""
